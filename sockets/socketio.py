245ca51cb2f5d708200618d4b5201c6a53f0050f
yuri-gagarin <ymivanov@icloud.com>
2022-06-18 10:23:11 +0300

    pyrightconfig.json

--- sockets/socketio.py
+++ sockets/socketio.py
@@ -1,6 +1,7 @@
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room
 from flask import request
 from storage.redis_controller import RedisController
+from typing import Dict
 
 class SocketIOServer:
   """
@@ -15,7 +16,7 @@ class SocketIOServer:
   def connection(self) -> None:
     """Connection handling"""
     @self.socketio.on("connect")
-    def handle_connection(data) -> None:
+    def handle_connection(data: str) -> None:
       """send a welcome message"""
       client_info = { 'socket_id': request.sid, 'namespace': request.namespace, 'message': "You are now connected live" } # type: ignore
       self.redisInstance.add_connected_client_info(request.sid) # type: ignore
@@ -29,6 +30,13 @@ class SocketIOServer:
       self.redisInstance.remove_connected_client_info(request.sid) # type: ignore
       print("Number of connected clients is: " + str(self.redisInstance.get_number_of_connected_clients()))
   
+  def room_handlers(self) -> None:
+    @self.socketio.on("join-room")
+    def join_room(data: Dict[str, str]) -> None:
+      print("Joining a room")
+      print(data)
+  
+
   def message_listeners(self) -> None:
     @self.socketio.on("message")
     def handle_message(message_data: str) -> None:
@@ -37,6 +45,7 @@ class SocketIOServer:
   def run(self) -> 'SocketIOServer':
     self.connection()
     self.message_listeners()
+    self.room_handlers()
     self.socketio.run(self.app)
     self.redisInstance.setup()
     return self
